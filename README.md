c716304e5af6bec2b3b07c4fe38b89a300863c20
supranove <j.othieno@gmail.com>
2013-07-08 11:19:20 +0200

    Added the MIT license to the project and additional regular expressions to validate the output folder name.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-A linux shell (bash) script used to download imgur albums. I wrote this specifically for 
-large albums because the 'download' option provided by imgur doesn't always work
-like it's supposed to.
+imgurdl
+=======
 
-To run, simply launch the script with the sole parameter being the URL of the
-album. For example, ./imgurdl http://imgur.com/a/wtwuu
+imgurdl is a small linux shell script used to download imgur albums. I wrote this
+specifically for large albums because the 'download' option provided by imgur doesn't
+always work like it's supposed to.
+
+The script relies on wget, grep, sed and some simple regular expressions.
+
+To run, simply launch the script with the sole parameter being the URL of the album.
+For example, __./imgurdl http://imgur.com/a/wtwuu__
 
 
 Tested on Debian 3.2.41-2 x86_64 GNU/Linux.
--- imgurdl.sh
+++ imgurdl.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2013 Jeremy Othieno.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 if [ $# -ne 1 ]
 then
-	echo "basename $0 <album url>"
+	echo "$0 <album url>, e.g. $0 http://imgur.com/a/wtwuu"
 	exit 1
 fi
 # The album's source URL. The trailing slash is removed and "/noscript" is added. When
@@ -15,8 +37,8 @@ URL_COUNT=$(wc -l <<< "$URLS")
 # If there're any images in the album, download them.
 if [ $URL_COUNT -gt 0 ]
 then
-	# Create the output directory.
-	DESTINATION=$(grep "<title>" <<< "$HTML" | sed 's/<title>//; s/[ \t]*//')
+	# Create the output directory, making sure to validate the filename.
+	DESTINATION=$(grep "<title>" <<< "$HTML" | sed 's/<title>//; s/[ \t]*//; s/\//_/g; s/\x0//g')
 	mkdir -p "$DESTINATION"
 	if [ -d "$DESTINATION" ]
 	then
